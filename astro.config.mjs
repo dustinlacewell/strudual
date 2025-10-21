38334557c67b59818be02ccfcb19f8dc7ac57aa9
Dustin Lacewell <dlacewell@gmail.com>
2025-10-20 23:09:52 -0500

    Fix url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ldlework.github.io',
-  base: '/strudual',
+  site: 'https://strudual.ldlework.com',
+  base: '/',
   integrations: [qwikdev(), tailwind()]
 });
